60d3e26ef7a3c39a77bfe687dffabe10ac940a40
md ismail <mdismail85773@gmail.com>
2025-09-20 15:57:28 +0800

    header and footer added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 import { createBrowserRouter } from "react-router";
+import Root from '../pages/Root/Root';
+import Error from '../pages/Root/Error/Error';
+import Home from '../pages/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component:Root,
+    errorElement:<Error></Error>,
+    children:[
+        {
+            index: true,
+            path:'/',
+            Component:Home
+        }
+    ]
   },
 ]);
 
